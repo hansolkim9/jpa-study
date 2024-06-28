374ae9d79bb889fd8530ec66e36bd83e8feaa88e
hansolkim9 <masicer2@gmail.com>
2024-06-28 11:48:11 +0900

    # 22. groupby, having - DTO 처리

--- src/test/java/com/spring/jpastudy/chap06_querydsl/repository/QueryDslGroupingTest.java
+++ src/test/java/com/spring/jpastudy/chap06_querydsl/repository/QueryDslGroupingTest.java
@@ -1,7 +1,11 @@
 package com.spring.jpastudy.chap06_querydsl.repository;
 
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.spring.jpastudy.chap06_querydsl.dto.GroupAverageAgeDto;
 import com.spring.jpastudy.chap06_querydsl.entity.Group;
 import com.spring.jpastudy.chap06_querydsl.entity.Idol;
 import org.junit.jupiter.api.BeforeEach;
@@ -106,6 +110,112 @@ class QueryDslGroupingTest {
         }
 
         System.out.println("\n\n");
+    }
+
+    @Test
+    @DisplayName("연령대별로 그룹화하여 아이돌 수를 조회")
+    void ageGroupTest() {
+        //given
+
+        // QueryDSL로 CASE WHEN THEN 표현식 만들기
+        NumberExpression<Integer> ageGroupExpression = new CaseBuilder()
+                .when(idol.age.between(10, 19)).then(10)
+                .when(idol.age.between(20, 29)).then(20)
+                .when(idol.age.between(30, 39)).then(30)
+                .otherwise(0);
+
+        //when
+        List<Tuple> result = factory
+                .select(ageGroupExpression, idol.count())
+                .from(idol)
+                .groupBy(ageGroupExpression)
+                .fetch();
+
+        //then
+        assertFalse(result.isEmpty());
+        for (Tuple tuple : result) {
+            int ageGroupValue = tuple.get(ageGroupExpression);
+            long count = tuple.get(idol.count());
+
+            System.out.println("\n\nAge Group: " + ageGroupValue + "대, Count: " + count);
+        }
+    }
+
+    @Test
+    @DisplayName("그룹별 평균 나이 조회")
+    void groupAverageAgeTest() {
+        //given
+
+         /*
+            SELECT G.group_name, AVG(I.age)
+            FROM tbl_idol I
+            JOIN tbl_group G
+            ON I.group_id = G.group_id
+            GROUP BY G.group_id
+            HAVING AVG(I.age) BETWEEN 20 AND 25
+         */
+
+        List<Tuple> result = factory
+                .select(idol.group.groupName, idol.age.avg())
+                .from(idol)
+                .groupBy(idol.group)
+                .having(idol.age.avg().between(20, 25))
+                .fetch();
+
+        // then
+        assertFalse(result.isEmpty());
+
+        System.out.println("\n\n");
+        for (Tuple tuple : result) {
+            String groupName = tuple.get(idol.group.groupName);
+            Double avgAge = tuple.get(idol.age.avg());
+
+            System.out.println(String.format("그룹명: %s, 평균 나이: %.2f", groupName, avgAge));
+        }
+        System.out.println("\n\n");
+
+    }
+
+    @Test
+    @DisplayName("그룹별 평균 나이 조회 (결과 DTO 처리)")
+    void groupAverageAgeDtoTest() {
+        //given
+
+         /*
+            SELECT G.group_name, AVG(I.age)
+            FROM tbl_idol I
+            JOIN tbl_group G
+            ON I.group_id = G.group_id
+            GROUP BY G.group_id
+            HAVING AVG(I.age) BETWEEN 20 AND 25
+         */
+
+        // Projections: 커스텀 DTO를 포장해주는 객체
+        List<GroupAverageAgeDto> result = factory
+                .select(
+                        Projections.constructor(
+                                GroupAverageAgeDto.class,
+                                idol.group.groupName,
+                                idol.age.avg()
+                        )
+                )
+                .from(idol)
+                .groupBy(idol.group)
+                .having(idol.age.avg().between(20, 25))
+                .fetch();
+
+        // then
+        assertFalse(result.isEmpty());
+
+        System.out.println("\n\n");
+        for (GroupAverageAgeDto tdto : result) {
+            String groupName = tdto.getGroupName();
+            Double avgAge = tdto.getAverageAge();
+
+            System.out.println(String.format("그룹명: %s, 평균 나이: %.2f", groupName, avgAge));
+        }
+        System.out.println("\n\n");
+
     }
 
 
